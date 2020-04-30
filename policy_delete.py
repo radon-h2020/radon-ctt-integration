9b4ab32d02877879ef83f9173f8df3403fb60fc1
Thomas F. Düllmann <duellmann@iste.uni-stuttgart.de>
2020-04-30 17:02:12 +0200

    Script improvement.

--- policy_delete.py
+++ policy_delete.py
@@ -8,7 +8,8 @@ file_param = sys.argv[1]
 with open(file_param, 'r') as input_file:
   tosca = yaml.load(input_file, Loader=yaml.FullLoader)
 
-del tosca['topology_template']['policies']
+if 'topology_template' in tosca and  'policies' in tosca['topology_template']:
+  del tosca['topology_template']['policies']
 
 with open(file_param, 'w') as output_file:
   yaml.dump(tosca, output_file)
