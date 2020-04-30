f72d4304a6a17eb811f425db984c904755af8f6a
Thomas F. Düllmann <duellmann@iste.uni-stuttgart.de>
2020-04-30 17:49:34 +0200

    Clean up all policies and policy_types with policy_delete script.

--- policy_delete.py
+++ policy_delete.py
@@ -1,18 +1,24 @@
 #!/usr/bin/env python3
 
+import glob
+import os
 import sys
 import yaml
 
-file_param = sys.argv[1]
+folder_param = sys.argv[1]
 
-with open(file_param, 'r') as input_file:
-  tosca = yaml.load(input_file, Loader=yaml.FullLoader)
+for file_param in glob.glob(folder_param + "/*.tosca"):
+  with open(file_param, 'r') as input_file:
+    tosca = yaml.load(input_file, Loader=yaml.FullLoader)
 
-if 'topology_template' in tosca and  'policies' in tosca['topology_template']:
-  del tosca['topology_template']['policies']
+  if 'policy_types' in tosca:
+    del tosca['policy_types']
 
-with open(file_param, 'w') as output_file:
-  yaml.dump(tosca, output_file)
+  if 'topology_template' in tosca and  'policies' in tosca['topology_template']:
+    del tosca['topology_template']['policies']
+
+  with open(file_param, 'w') as output_file:
+    yaml.dump(tosca, output_file)
 
 
 
