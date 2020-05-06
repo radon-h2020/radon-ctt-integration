d64e1d8f7691ea66bf1d62391e5033ab9adbdc34
Thomas F. Düllmann <duellmann@iste.uni-stuttgart.de>
2020-05-06 16:29:38 +0200

    Strip quotes from curl_uuid output.

--- curl_uuid.sh
+++ curl_uuid.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-curl -X POST "$1" -H "accept: */*" -H "Content-Type: application/json" -d "$2" | jq .uuid 
+curl -X POST "$1" -H "accept: */*" -H "Content-Type: application/json" -d "$2" | jq .uuid | tr -d \" 
