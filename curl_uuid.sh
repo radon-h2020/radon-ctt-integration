431d793359453f5c07467b4e08c4895b3bb79142
Thomas F. Düllmann <duellmann@iste.uni-stuttgart.de>
2020-05-06 16:09:12 +0200

    Updated curl_uuid script.

--- curl_uuid.sh
+++ curl_uuid.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-echo $(curl -X POST "$1" -H 'accept: */*' -H 'Content-Type: application/json' -d "$2" | jq .uuid)
+curl -X POST "$1" -H "accept: */*" -H "Content-Type: application/json" -d "$2" | jq .uuid 
