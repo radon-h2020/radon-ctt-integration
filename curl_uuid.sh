9fce78aa70e958e266d3cd01bb7eb689a7de9764
Thomas F. Düllmann <duellmann@iste.uni-stuttgart.de>
2020-07-06 15:18:03 +0200

    Added debug output to curl_uuid script.

--- curl_uuid.sh
+++ curl_uuid.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
+echo "Param \$1: $1"
+echo "Param \$2: $2"
+echo "Param \$3: $3"
+
 curl -X POST "$1" -H "accept: */*" -H "Content-Type: application/json" -d "$2" | jq $3 | tr -d \" 
